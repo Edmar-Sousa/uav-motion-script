a7d5c9a0e2697ad9d171771f39abfbb468576298
Edmar_Sousa <61367147+Edmar-Sousa@users.noreply.github.com>
2026-04-05 17:07:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from uav.controllers import TrajectoryPlanner, PositionController, AltitudeController, MotorController
+from uav.vant import Vant
+from uav.draw import DrawVant
+
+
+
+def main():
+    osm_file_path = './map.osm'
+
+    
+
+    # trajectory_planner = TrajectoryPlanner()
+
+    # position_controller = PositionController(trajectory_planner)
+    # altitude_controller = AltitudeController(position_controller)
+    # motor_controller = MotorController(position_controller, altitude_controller)
+    
+    # vant = Vant(
+    #     motor_controller=motor_controller,
+    #     position_controller=position_controller,
+    #     altitude_controller=altitude_controller
+    # )
+    # draw_vant = DrawVant(vant)
+    
+    # draw_vant.show()
+    pass
+
+
+if __name__ == '__main__':
+    main()
