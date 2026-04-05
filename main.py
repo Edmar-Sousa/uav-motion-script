a45c5404af8de95b0da55a13978dc7bb88546155
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-04-05 18:01:21 -0300

    feat: logic to check file

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 
-from uav.controllers import TrajectoryPlanner, PositionController, AltitudeController, MotorController
+import argparse
+
+from uav.trajectory import TrajectoryPlanner
+from uav.controllers import PositionController, AltitudeController, MotorController
 from uav.vant import Vant
 from uav.draw import DrawVant
 
 
+def get_args_script():
+    parser = argparse.ArgumentParser(
+        prog='UAV motion',
+        description='Script to simulate UAV motion to Food Delivery'
+    )
+
+    parser.add_argument('--osm')
+    args = parser.parse_args()
+
+    return args
+
 
 def main():
-    osm_file_path = './map.osm'
+    args = get_args_script()
 
-    
+    trajectory_planner = TrajectoryPlanner(args.osm)
+    trajectory_planner.generate()
 
-    trajectory_planner = TrajectoryPlanner()
-
-    position_controller = PositionController(trajectory_planner)
-    altitude_controller = AltitudeController(position_controller)
-    motor_controller = MotorController(position_controller, altitude_controller)
+    # position_controller = PositionController(trajectory_planner)
+    # altitude_controller = AltitudeController(position_controller)
+    # motor_controller = MotorController(position_controller, altitude_controller)
     
-    vant = Vant(
-        motor_controller=motor_controller,
-        position_controller=position_controller,
-        altitude_controller=altitude_controller
-    )
-    draw_vant = DrawVant(vant)
+    # vant = Vant(
+    #     motor_controller=motor_controller,
+    #     position_controller=position_controller,
+    #     altitude_controller=altitude_controller
+    # )
+    # draw_vant = DrawVant(vant)
     
-    draw_vant.show()
+    # draw_vant.show()
 
 
 if __name__ == '__main__':
--- uav/trajectory.py
+++ uav/trajectory.py
@@ -1,3 +1,18 @@
+import osmium
+import os
 
 
+class OsmHandler(osmium.SimpleHandler):
+    pass
 
+
+class TrajectoryPlanner:
+    def __init__(self, osm_path: str):
+        self.osm_path = osm_path
+
+    def __check_file_osm_exists__(self):
+        if not os.path.isfile(self.osm_path):
+            raise Exception('OSM file not exists')
+
+    def generate(self):
+        self.__check_file_osm_exists__()
