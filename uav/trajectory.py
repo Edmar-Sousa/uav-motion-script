d3b9a60ba61f601cbad3e7429825e0882f14e7a3
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-04-05 18:10:31 -0300

    feat: parse osm

--- uav/trajectory.py
+++ uav/trajectory.py
@@ -3,16 +3,22 @@ import os
 
 
 class OsmHandler(osmium.SimpleHandler):
-    pass
+    def node(self, n):
+        print(n.location.lat, n.location.lon)
 
 
 class TrajectoryPlanner:
     def __init__(self, osm_path: str):
         self.osm_path = osm_path
+        self.osm_handler = OsmHandler()
+
 
     def __check_file_osm_exists__(self):
         if not os.path.isfile(self.osm_path):
             raise Exception('OSM file not exists')
 
+
     def generate(self):
         self.__check_file_osm_exists__()
+
+        self.handler_osm.apply_file(self.osm_path)
