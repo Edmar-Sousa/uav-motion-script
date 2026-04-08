80181a7df40b73c523a30f1f0e27e4358c370cde
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-04-08 01:04:12 -0300

    feat: generate random point do destination delivery

--- uav/trajectory.py
+++ uav/trajectory.py
@@ -1,7 +1,9 @@
 import osmium
-import os
-
 import folium
+import os
+import numpy
+import random
+import pyproj
 
 
 class OsmHandler(osmium.SimpleHandler):
@@ -66,7 +68,16 @@ class TrajectoryPlanner:
         self.osm_path = osm_path
         self.osm_handler = OsmHandler(self.callback)
 
+        self.geod = pyproj.Geod(ellps="WGS84")
         self.map = folium.Map(location=[-4.9688697, -39.017628], zoom_start=10)
+
+
+    def __generate_destination_point__(self, lat, long, max_distance = 2):
+        degree = random.randint(0, 360)
+        distance = max_distance * 1000 * numpy.sqrt(random.random())
+
+        long2, lat2, _ = self.geod.fwd(long, lat, degree, distance)
+        return lat2, long2
 
 
     def __check_file_osm_exists__(self):
@@ -76,10 +87,17 @@ class TrajectoryPlanner:
     
     def callback(self, position, tags):
         name = tags['name'] if 'name' in tags.keys() else tags['comment']
+        delivery_destination = self.__generate_destination_point__(position[0], position[1])
         
         folium.Marker(
             location=position,
             popup=name
+        ).add_to(self.map)
+
+        folium.Marker(
+            location=delivery_destination,
+            popup=f'{name} - dest',
+            icon=folium.Icon(color="red")
         ).add_to(self.map)
 
         self.map.save('trajectory.html')
